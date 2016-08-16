67fc814d79b210348f724903d2da498b4e9b45f4
Marcellin Nshimiyimana <mars@fusemachines.com>
2016-08-16 21:34:52 +0545

    create skeleton presentation

--- javascript-intro-08-11-2016/demo/util-self-contained-block.js
+++
@@ -1,9 +0,0 @@
-/**
- * email must have an "AT" and "DOT" characters
- * @param input { String }
- * @namespace inputValidators.isEmail
- * @module sam-ai-ui
- */
-export function isEmail(input) {
-	return !!input && input.split('@').length > 1 && input.split('\.').length > 1;
-}
